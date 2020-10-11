6ff59c5ec8fbe299cde52543c49ae0e9526a8673
nhatvu148 <nhatvu148@gmail.com>
2020-10-11 17:54:33 +0900

    add LINQ

---
+++ Assets/Linq.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class Linq : MonoBehaviour
+{
+    public string[] names = { "jon", "jon", "julie", "alex", "julie", "jessie", "david", "mark", "matt"};
+    // Start is called before the first frame update
+    void Start()
+    {
+        var nameFound = names.Any(name => name == "jon");
+        Debug.Log(nameFound);
+
+        var nameDistinct = names.Distinct();
+        foreach (string name in nameDistinct)
+        {
+         //   Debug.Log(name);
+        }
+
+        var result = names.Where(n => n.Length > 5);
+        foreach (string name in result)
+        {
+            Debug.Log(name);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
